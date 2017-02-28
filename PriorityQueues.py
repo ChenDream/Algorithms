735eff6b2c3beaa9a9e3dc362834d65076b94985
ChenDream <lingchen1991@hotmail.com>
2017-02-28 23:33:05 +0800

    separate heap from heapsort file, add min heap

--- PriorityQueues.py
+++ PriorityQueues.py
@@ -1,6 +1,7 @@
-import HeapSort
-from HeapSort import Heap,MaxHeap
 
+from Heap import MinHeap,MaxHeap
+
+# class PriorityQueues():
 
 if __name__ == "__main__":
     b = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
