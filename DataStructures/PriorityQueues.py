814142007d22f23d3f828a037b98d4c7bcda6412
ChenDream <lingchen1991@hotmail.com>
2017-03-11 13:56:02 +0800

    finish priority queue with maxheap, TODO: minheap has bug to fix

--- DataStructures/PriorityQueues.py
+++ DataStructures/PriorityQueues.py
@@ -1,13 +1,42 @@
-
-from SortingAndOrderStatistics.Heap import MaxHeap
+import random
+from SortingAndOrderStatistics.Heap import MaxHeap,MinHeap
 from DataStructures.Queues import Queue
 #implement first-in first-out with priority queue
 #implement stack with priority queue
 
-# class PriorityQueues():
-
+#TODO: minheap has bug that pop will return int.MIN number
+class PriorityQueues():
+    def __init__(self,type=max):
+        if type == max:
+            self.type = True
+        else:
+            self.type = False
+        if self.type:
+            self.maxheap = MaxHeap([])
+        else:
+            self.minheap = MinHeap([])
+    def pop(self):
+        if self.type:
+            return self.maxheap.HeapExtractMax()
+        else:
+            return self.minheap.HeapExtractMin()
+    def push(self,x):
+        if self.type:
+            self.maxheap.MaxHeapInsert(x)
+        else:
+            self.minheap.MinHeapInsert(x)
+    def top(self):
+        if self.type:
+            return self.maxheap.HeapMaximum()
+        else:
+            return self.minheap.HeapMinimum()
 if __name__ == "__main__":
-    b = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
-    maxheap = MaxHeap(b)
-    maxheap.BuildHeap()
-    print(maxheap)
+    pq = PriorityQueues()
+    lst = []
+    for i in range(15):
+        i = random.randint(1,100)
+        lst.append(i)
+        pq.push(i)
+    for i in range(15):
+        print(pq.pop())
+    print(lst)
