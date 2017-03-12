ea1e959bd6497404caa6a56c20df94cc2fbd5538
ChenDream <lingchen1991@hotmail.com>
2017-03-12 21:27:24 +0800

    linedlist

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -1,6 +1,14 @@
 
 class DoubleLinkedList():
-    def __init__(self):
-        self.key = 0
+    def __init__(self,key):
+        self.key = key
         self.prev = None
         self.next = None
+
+    def search(self,key):
+        x = self
+        while x != None and x.key != key:
+            x = x.next
+        return x
+    def insert(self,x):
+        x.next = self
