013ea35c6b46a4b15d0a54590f9019093353528b
ChenDream <lingchen1991@hotmail.com>
2017-03-14 17:24:27 +0800

    add

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -1,14 +1,28 @@
 
-class DoubleLinkedList():
+class DoubleLinkedList:
     def __init__(self,key):
-        self.key = key
-        self.prev = None
-        self.next = None
+        self.head = None
 
     def search(self,key):
         x = self
         while x != None and x.key != key:
             x = x.next
         return x
+
     def insert(self,x):
         x.next = self
+
+
+class LinkNode:
+    def __init__(self, key):
+        self.key = key
+        self.prev = None
+        self.next = None
+
+    def getkey(self):
+        return self.key
+
+if __name__ == "__main__":
+
+    doublelist = DoubleLinkedList
+
