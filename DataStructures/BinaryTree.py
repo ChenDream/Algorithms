61fffc0182ca7743f4fa62cb2fb07f6e36756a5e
ChenDream <lingchen1991@hotmail.com>
2017-03-19 20:41:38 +0800

    add random insert

--- DataStructures/BinaryTree.py
+++ DataStructures/BinaryTree.py
@@ -1,4 +1,4 @@
-
+import random
 class Node:
     def __init__(self,key):
         self.key = key
@@ -11,8 +11,23 @@ class BinaryTree:
         self.root =None
 
     # random insert
-    def insert(self,key):
+    def randomInsert(self,key):
         node = Node(key)
         if self.root == None:
             self.root = node
             return
+        leaf = self.root
+        while leaf != None:
+            r = random.randint(1,2)
+            if r == 1:
+                leaf = leaf.left
+            else:
+                leaf = leaf.right
+        leaf = node
+
+if __name__ == "__main__":
+    bt = BinaryTree()
+    for i in range(13):
+        bt.randomInsert(i)
+    print("Done")
+
